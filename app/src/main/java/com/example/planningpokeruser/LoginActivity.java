feabcbcd439e6a3ce49a5ee3336f1fe9cdf18e7b
huszarhuni <h.huni56@freemail.hu>
2019-12-09 02:03:50 +0200

    Mukodik a szavazas

--- app/src/main/java/com/example/planningpokeruser/LoginActivity.java
+++ app/src/main/java/com/example/planningpokeruser/LoginActivity.java
@@ -2,13 +2,44 @@ package com.example.planningpokeruser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    EditText editTextCode;
+    Button buttonEnter;
+
+    String code;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        initialize();
+
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                code = editTextCode.getText().toString();
+                name = editTextCode.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), VoteActivity.class);
+                intent.putExtra("codeString",code);
+                intent.putExtra("nameString",name);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialize() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextCode = findViewById(R.id.editTextCode);
+        buttonEnter = findViewById(R.id.buttonEnter);
     }
 }
--- app/src/main/java/com/example/planningpokeruser/VoteActivity.java
+++ app/src/main/java/com/example/planningpokeruser/VoteActivity.java
@@ -2,13 +2,59 @@ package com.example.planningpokeruser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class VoteActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    String code;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        initialize();
+
+        code = getIntent().getStringExtra("codeString");
+        name = getIntent().getStringExtra("nameString");
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.child("Aktiv").child("123").child("Szavazatok").child("1").setValue(name);
+            }
+        });
+
+
     }
+
+    private void initialize() {
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+    }
+
 }
